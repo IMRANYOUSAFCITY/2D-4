86575607aa75916f73ee6e6b8ebfae86468a6af5
IMRANYOUSAFCITY <124051833+IMRANYOUSAFCITY@users.noreply.github.com>
2024-04-27 19:13:33 +0100

    working code

--- src/TemporaryNode.java
+++ src/TemporaryNode.java
@@ -103,11 +103,7 @@ public class TemporaryNode implements TemporaryNodeInterface {
                 if(value != null){
                     return value;
                 }
-                if(i == nodes.length-2){
-                    get(key);
-                }else {
-                    end();
-                }
+                end();
             }
             return null;
         } catch (Exception e) {
